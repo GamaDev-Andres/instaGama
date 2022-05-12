2f0ed4f5ae24fb8b2f1de8e52fe12fc9f6131516
Andres Gama <gama_pipe@hotmail.com>
2022-05-11 22:02:36 -0500

    fix bug dependencia

--- src/pages/pageComents/hook/usePageComentContext.jsx
+++ src/pages/pageComents/hook/usePageComentContext.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react/cjs/react.development';
+import { useContext } from 'react';
 import pageComentContext from '../context/pageComentContext';
 
 const usePageComentContext = () => {
